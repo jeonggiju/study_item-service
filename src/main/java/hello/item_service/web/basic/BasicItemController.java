557f9d4dd59227dfa321da65a5cc1296896192f0
jeonggiju <halogiju123@gmail.com>
2025-01-20 16:08:49 +0900

    20250120

--- src/main/java/hello/item_service/web/basic/BasicItemController.java
+++ src/main/java/hello/item_service/web/basic/BasicItemController.java
@@ -60,14 +60,59 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+    /**
+     * @ModelAttribute("item") Item item
+     * model.addAttribute("item", item); 자동 추가
+     */
+//    @PostMapping("/add")
     public String addItemV2(
-            @ModelAttribute("item") Item item, Model model
+            @ModelAttribute("item") Item item
     ){
         itemRepository.save(item);
 //        model.addAttribute("item", item); <- ModelAttribute는 ("xxx")를 addAttribute에 넣어준다.
+         return "basic/item";
+    }
 
+    /**
+     * @ModelAttribute name 생략 가능
+     * model.addAttribute(item); 자동 추가, 생략 가능
+     * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
+     */
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+//        class 명인 Item 에서 첫글자만 소문자로 등록 item
+//        model.addAttribute("item", item);
+        itemRepository.save(item);
         return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute 자체 생략 가능
+     * model.addAttribute(item) 자동 추가
+     */
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(
+            @PathVariable("itemId") Long itemId,
+            Model model
+    ){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(
+            @PathVariable("itemId") Long itemId,
+            @ModelAttribute Item item
+    ){
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
     }
 
     /**
